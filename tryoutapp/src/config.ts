e990b41e0a58fdcaa0d8e57e91455144b44ee84c
lamhot01 <139417025+lamhot01@users.noreply.github.com>
2026-05-05 18:17:09 +0700

    move asset from src to public

--- tryoutapp/src/config.ts
+++ tryoutapp/src/config.ts
@@ -1,5 +1,5 @@
 // Mengambil URL dari .env, jika tidak ada gunakan fallback localhost
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8002/api";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const API_ENDPOINTS = {
   TEST_INFO: (id: number) => `${BASE_URL}/test-info/${id}`,
